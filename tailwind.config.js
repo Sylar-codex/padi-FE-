a4389c10419cba276a4f348d6955b2c0e4d6ef71
sylar-codex <vicaremy@gmail.com>
2023-04-09 01:37:50 +0100

    feature

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    colors: {
+      white: "#ffffff",
+      "info-10": "#F0F6FF",
+      "gray-5": "#F9F9F9",
+      "gray-10": "#F4F7FB",
+      "gray-20": "#EAEEF5",
+      "gray-30": "#D7DEEA",
+      "gray-40": "#B7BECD",
+      "gray-50": "#9DA5B4",
+      "gray-60": "#8690A2",
+      "gray-70": "#646E82",
+      "gray-80": "#4B5362",
+      "gray-90": "#383D48",
+      "gray-100": "#21242C",
+      "primary-40": "#30D5A5",
+      "primary-50": "#E8FFF8",
+    },
+  },
+  plugins: [],
+};
